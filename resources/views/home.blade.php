3b6ffe96638c0a8387a2f38112b26380b8156d4c
Miguel Pazo <miguel.ps19@gmail.com>
2020-07-14 13:46:10 -0500

    update result

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>RENIEC IDaaS - Example PHP</title>
+    <title>ID Perú - Example PHP</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -12,16 +12,12 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-12">
-            <h1 class="text-center">RENIEC IDaaS</h1>
+            <h1 class="text-center">ID Perú</h1>
             <h4 class="text-center">Auth Result</h4>
 
             <div class="col-sm-12">
                 <p><b>DNI:</b> {{ $oUser->doc }}</p>
                 <p><b>Primer nombre:</b> {{ $oUser->first_name }}</p>
-                <p><b>Teléfono:</b> {{ $oUser->phone_number }}</p>
-                <p><b>Teléfono verificado:</b> {{ $oUser->phone_number_verified }}</p>
-                <p><b>Email:</b> {{ $oUser->email }}</p>
-                <p><b>Email verificado:</b> {{ $oUser->email_verified }}</p>
                 <p><b>Sub:</b> {{ $oUser->sub }}</p>
             </div>
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>RENIEC IDaaS - Example PHP</title>
+    <title>ID Perú - Example PHP</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -12,7 +12,7 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-12">
-            <h1 class="text-center">RENIEC IDaaS</h1>
+            <h1 class="text-center">ID Perú</h1>
             <h4 class="text-center">PHP Example Integration</h4>
 
             <div class="col-sm-12 text-center">
